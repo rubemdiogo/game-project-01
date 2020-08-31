d12676360e644ae033a707a8242bdacde8da4a41
Rubem Diogo <rubemdiogo@gmail.com>
2020-08-31 13:20:50 -0400

    add background

--- js/game.js
+++ js/game.js
@@ -1,8 +1,38 @@
+// origem da imag dos sprites
 const sprites = new Image();
 sprites.src = "/images/1sprite.png";
 
+// origem da img do background
+
+const back = new Image();
+back.src = "/images/background.png";
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
+
+
+//propriedades do background
+let mapa = {
+  spriteX: 0,
+  spriteY: 0,
+  largura: 1056,
+  altura: 672,
+  larguraTela: 1200,
+  alturaTela: 600,
+  x: 0,
+  y: 0,
+  desenha() {
+    ctx.drawImage(
+      back,
+      mapa.spriteX, mapa.spriteY,
+      mapa.largura, mapa.altura,
+      mapa.x, mapa.y,
+      mapa.larguraTela, mapa.alturaTela,
+    );
+  }
+}
+
 
 
 //propriedades do sprite
@@ -13,8 +43,8 @@ let player = {
   altura: 46,
   larguraTela: 100,
   alturaTela: 100,
-  x: 50,
-  y: 400,
+  x: 100,
+  y: 400, // 400 é o valor do chao 
   desenha() {
     ctx.drawImage(
       sprites,
@@ -28,7 +58,9 @@ let player = {
 
 
 function loop() {
+  mapa.desenha();
   player.desenha();
+
   requestAnimationFrame(loop);
 }
 loop();
